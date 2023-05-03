d73b53f217489402d220a9624820b5abf4b85480
Pavanhimansu <pavan.alapati63@gmail.com>
2023-05-03 12:39:57 +0530

    setupProject

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,27 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('/');
+    cy.get('[data-cy="driver"]').click();
+
+    cy.get('[data-cy="driverName"]').find('input').type('Pavan Alapati');
+    cy.get('[data-cy="aadharCard"]').find('input').type('479647177584');
+    cy.get('[data-cy="DrivingLiasnace"]').find('input').type('AP03720120034865');
+    cy.get('[data-cy="ContactCell"]').find('input').type('9533852285');
+    cy.get('[data-cy="phonePayNo"]').find('input').type('9533852285');
+
+    cy.get('[data-cy="Register"]').click();
+
+    cy.get('[data-cy="driverDetails"]').click();
+
+    
+
+
+  })
+})
+
+//driverName
+//aadharCard
+//DrivingLiasnace
+//ContactCell
+//phonePayNo
+// Register
